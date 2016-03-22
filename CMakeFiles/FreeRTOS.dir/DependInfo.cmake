08d8a9ccb0b1d3130d7b98d4c6a35d5cddc76a0e
ATmega8 <295017359@qq.com>
2016-03-22 10:36:21 +0000

    FreeRTOS Submodel

---
+++ CMakeFiles/FreeRTOS.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/life/Code/FreeRTOSV8.2.1/FreeRTOS/src/croutine.c" "/home/life/Code/FreeRTOSV8.2.1/FreeRTOS/CMakeFiles/FreeRTOS.dir/src/croutine.c.o"
+  "/home/life/Code/FreeRTOSV8.2.1/FreeRTOS/src/event_groups.c" "/home/life/Code/FreeRTOSV8.2.1/FreeRTOS/CMakeFiles/FreeRTOS.dir/src/event_groups.c.o"
+  "/home/life/Code/FreeRTOSV8.2.1/FreeRTOS/src/heap_3.c" "/home/life/Code/FreeRTOSV8.2.1/FreeRTOS/CMakeFiles/FreeRTOS.dir/src/heap_3.c.o"
+  "/home/life/Code/FreeRTOSV8.2.1/FreeRTOS/src/list.c" "/home/life/Code/FreeRTOSV8.2.1/FreeRTOS/CMakeFiles/FreeRTOS.dir/src/list.c.o"
+  "/home/life/Code/FreeRTOSV8.2.1/FreeRTOS/src/port.c" "/home/life/Code/FreeRTOSV8.2.1/FreeRTOS/CMakeFiles/FreeRTOS.dir/src/port.c.o"
+  "/home/life/Code/FreeRTOSV8.2.1/FreeRTOS/src/queue.c" "/home/life/Code/FreeRTOSV8.2.1/FreeRTOS/CMakeFiles/FreeRTOS.dir/src/queue.c.o"
+  "/home/life/Code/FreeRTOSV8.2.1/FreeRTOS/src/tasks.c" "/home/life/Code/FreeRTOSV8.2.1/FreeRTOS/CMakeFiles/FreeRTOS.dir/src/tasks.c.o"
+  "/home/life/Code/FreeRTOSV8.2.1/FreeRTOS/src/timers.c" "/home/life/Code/FreeRTOSV8.2.1/FreeRTOS/CMakeFiles/FreeRTOS.dir/src/timers.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/FreeRTOS.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FreeRTOS.dir/src/tasks.c.o"
+  "CMakeFiles/FreeRTOS.dir/src/queue.c.o"
+  "CMakeFiles/FreeRTOS.dir/src/heap_3.c.o"
+  "CMakeFiles/FreeRTOS.dir/src/timers.c.o"
+  "CMakeFiles/FreeRTOS.dir/src/event_groups.c.o"
+  "CMakeFiles/FreeRTOS.dir/src/port.c.o"
+  "CMakeFiles/FreeRTOS.dir/src/croutine.c.o"
+  "CMakeFiles/FreeRTOS.dir/src/list.c.o"
+  "libFreeRTOS.pdb"
+  "libFreeRTOS.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FreeRTOS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
