5a5aaf5f9263d4b21e0b08c0ad803ab77e9baa95
ATmega8 <liuyang.2014@foxmail.com>
2016-04-11 16:50:26 +0000

    修改中断优先级为正确的中断优先级

--- inc/FreeRTOSConfig.h
+++ inc/FreeRTOSConfig.h
@@ -152,10 +152,10 @@ PRIORITY THAN THIS! (higher priorities are lower numeric values. */
 
 /* Interrupt priorities used by the kernel port layer itself.  These are generic
 to all Cortex-M ports, and do not rely on any particular library functions. */
-#define configKERNEL_INTERRUPT_PRIORITY 		5 /*( configLIBRARY_LOWEST_INTERRUPT_PRIORITY << (8 - configPRIO_BITS) )*/
+#define configKERNEL_INTERRUPT_PRIORITY  0x50
 /* !!!! configMAX_SYSCALL_INTERRUPT_PRIORITY must not be set to zero !!!!
 See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html. */
-#define configMAX_SYSCALL_INTERRUPT_PRIORITY 	3 /*( configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY << (8 - configPRIO_BITS) )*/
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY  0x30
 	
 /* Normal assert() semantics without relying on the provision of an assert.h
 header file. */
